d1a0dfc307e7bb93bdb73bd5be8b892d58b5f51c
jmonaste <javiermonasterio@gmail.com>
2017-04-01 00:01:48 +0200

    Add files via upload

---
+++ src/main/java/com/introtoandroid/samplematerial/CardBTK_Vehicule_Basic_Info.java
@@ -0,0 +1,52 @@
+package com.introtoandroid.samplematerial;
+
+
+public class CardBTK_Vehicule_Basic_Info {
+
+
+    private String vehiculeCode;
+    private String vehiculeName;
+    private String vehiculeRegNumber;
+    private String vehiculeType;
+    private int color_resource;
+
+    public String getVehiculeName() {
+        return vehiculeName;
+    }
+
+    public void setVehiculeName(String vehiculeName) {
+        this.vehiculeName = vehiculeName;
+    }
+
+    public String getVehiculeCode() {
+        return vehiculeCode;
+    }
+
+    public void setVehiculeCode(String vehiculeCode) {
+        this.vehiculeCode = vehiculeCode;
+    }
+
+    public String getVehiculeRegNumber() {
+        return vehiculeRegNumber;
+    }
+
+    public void setVehiculeRegNumber(String vehiculeRegNumber) {
+        this.vehiculeRegNumber = vehiculeRegNumber;
+    }
+
+    public String getVehiculeType() {
+        return vehiculeType;
+    }
+
+    public void setVehiculeType(String vehiculeType) {
+        this.vehiculeType = vehiculeType;
+    }
+
+    public int getColor_resource() {
+        return color_resource;
+    }
+
+    public void setColor_resource(int color_resource) {
+        this.color_resource = color_resource;
+    }
+}
